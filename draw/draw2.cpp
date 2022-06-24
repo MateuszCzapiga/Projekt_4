583d6a697ea2006cc00bc3983aee5447e716e4ad
Mateusz Czapiga <s188882@o365.student.pg.edu.pl>
2022-06-24 15:45:23 +0200

    ->global info, moving

--- draw/draw2.cpp
+++ draw/draw2.cpp
@@ -5,6 +5,7 @@
 #include "draw2.h"
 #include <vector>
 #include <cstdio>
+#include <iostream>
 
 #define MAX_LOADSTRING 100
 #define TMR_1 1
@@ -19,11 +20,19 @@ INT value;
 // buttons
 HWND hwndButton;
 
-// sent data
 int col = 0;
+int polozenie_windy = 0;
+int podloga = 0;
+int pietro_2 = -128;
+int pietro_3 = -256;
+int pietro_4 = -384;
+int pietro_5 = -512; 
+
+
 std::vector<Point> data;
 RECT drawArea1 = { 0, 0, 150, 200 };
 RECT drawArea2 = { 50, 400, 650, 422};
+RECT winda = { 710, 70, 890, 720 };
 
 // Forward declarations of functions included in this code module:
 ATOM				MyRegisterClass(HINSTANCE hInstance);
@@ -49,6 +58,9 @@ void Rysuj_Winde(HDC hdc)
 {
 	Graphics graphics(hdc);
 	Pen obramowanie(Color(255, 0, 0, 0));
+	Pen zakres_windy(Color(255, 255, 0, 0));
+	Pen winda(Color(255, 0, 0, 255));
+
 	//obramowanie
 	graphics.DrawLine(&obramowanie, 700, 50, 700, 750); // lewa linia
 	graphics.DrawLine(&obramowanie, 900, 50, 900, 750); // prawa linia
@@ -60,32 +72,36 @@ void Rysuj_Winde(HDC hdc)
 	graphics.DrawLine(&obramowanie, 700, 464, 600, 464); // 3.pietro
 	graphics.DrawLine(&obramowanie, 900, 592, 1000, 592); // 2.pietro
 	graphics.DrawLine(&obramowanie, 700, 720, 600, 720); // 1.pietro
+	//zakres windy
+	graphics.DrawLine(&zakres_windy, 710, 80, 890, 80); // gora
+	graphics.DrawLine(&zakres_windy, 710, 720, 890, 720); // dol
+	graphics.DrawLine(&zakres_windy, 710, 80, 710, 720); // lewa
+	graphics.DrawLine(&zakres_windy, 890, 80, 890, 720); // prawa
+	// winda 180X100
+	//polozenie_windy = pietro_4;
+	graphics.DrawLine(&winda, 710, 720 + polozenie_windy, 890, 720 + polozenie_windy); // dol
+	graphics.DrawLine(&winda, 710, 620 + polozenie_windy, 890, 620 + polozenie_windy); // gora
+	graphics.DrawLine(&winda, 710, 720 + polozenie_windy, 710, 620 + polozenie_windy); // lewa
+	graphics.DrawLine(&winda, 890, 720 + polozenie_windy, 890, 620 + polozenie_windy); // prawa
 }
 
-void repaintWindow(HWND hWnd, HDC &hdc, PAINTSTRUCT &ps, RECT *drawArea)
+void Przerysuj_Winde(HWND hWnd, HDC& hdc, PAINTSTRUCT& ps, RECT* drawArea, int przesuniecie)
 {
-	if (drawArea==NULL)
+	
+	if (drawArea == NULL)
 		InvalidateRect(hWnd, NULL, TRUE); // repaint all
 	else
 		InvalidateRect(hWnd, drawArea, TRUE); //repaint drawArea
+	EndPaint(hWnd, &ps);
 	hdc = BeginPaint(hWnd, &ps);
-	//MyOnPaint(hdc);
+
+	Rysuj_Winde(hdc);
 	EndPaint(hWnd, &ps);
 }
 
-void inputData()
-{	
-	data.push_back(Point(0, 0));
-	for (int i = 1; i < 100; i++){
-		data.push_back(Point(2*i+1, 200 * rand()/RAND_MAX));
-	}
-}
-
-
-int OnCreate(HWND window)
+void Pisz_1(HDC hdc, int przesuniecie)
 {
-	inputData();
-	return 0;
+	TextOut(hdc, 10 + przesuniecie, 10, TEXT("1"), strlen("1"));
 }
 
 
@@ -417,7 +433,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 		(HMENU)ID_5_4,                   // the ID of your button
 		hInstance,                            // the instance of your application
 		NULL);                               // extra bits you dont really need 
-	OnCreate(hWnd);
+	
 
 	if (!hWnd)
 	{
@@ -462,15 +478,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		case IDM_EXIT:
 			DestroyWindow(hWnd);
 			break;
-		case ID_BUTTON1 :
-			col++;
-			if (col > 10)
-				col = 0;
-			repaintWindow(hWnd, hdc, ps, &drawArea1);
-			break;
-		case ID_BUTTON2 :
-			repaintWindow(hWnd, hdc, ps, NULL);
-			break;
+		case ID_1_2 :
+			polozenie_windy = podloga + pietro_2;
+			Przerysuj_Winde(hWnd, hdc, ps, &winda, polozenie_windy);
+		case ID_1_3:
+			polozenie_windy = podloga + pietro_3;
+			Przerysuj_Winde(hWnd, hdc, ps, &winda, polozenie_windy);
 		default:
 			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
@@ -479,6 +492,20 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		hdc = BeginPaint(hWnd, &ps);
 		// TODO: Add any drawing code here (not depend on timer, buttons)
 		Rysuj_Winde(hdc);
+
+
+
+		//PAINTSTRUCT ps;
+		//
+		//for (int i = 1; i <= 4; i++)
+		//{
+		//	int przesuniecie = i * 10;
+		//	Pisz_1(hdc, przesuniecie);
+		//}
+		////MessageBoxA(NULL, buffer, "Lesson1", MB_OK);
+
+
+
 		EndPaint(hWnd, &ps);
 		break;
 	case WM_DESTROY:
@@ -490,7 +517,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		{
 		case TMR_1:
 			//force window to repaint
-			repaintWindow(hWnd, hdc, ps, &drawArea2);
+			//repaintWindow(hWnd, hdc, ps, &drawArea2);
 			value++;
 			break;
 		}
